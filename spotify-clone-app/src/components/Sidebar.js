49c367a8b4e19538dbe4f1f191f17edff2746eca
poem <normamoras@gmail.com>
2020-08-20 16:02:54 -0400

    adding material UI icons

--- spotify-clone-app/src/components/Sidebar.js
+++ spotify-clone-app/src/components/Sidebar.js
@@ -1,12 +1,18 @@
 import React from 'react'
-import './Body.css'
+import './Sidebar.css'
+import SidebarOption from './SidebarOption'
+
 
 function Sidebar() {
     return (
-        <div className="body">
-            <img src="https://getheavy.com/wp-content/uploads/2019/12/spotify2019-830x350.jpg"
+        <div className="sidebar">
+            <img className="sidebar_logo" src="https://getheavy.com/wp-content/uploads/2019/12/spotify2019-830x350.jpg"
             alt=""
             />
+
+            <SidebarOption title="Home" />
+            <SidebarOption title="search" />
+            <SidebarOption title="Your Library" />
             
         </div>
     )
---
+++ spotify-clone-app/src/components/SidebarOption.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function SidebarOption({ title, Icon }) {
+    return (
+        <div className="sidebarOption">
+            <p>
+                
+            </p>
+            
+        </div>
+    )
+}
+
+export default SidebarOption
