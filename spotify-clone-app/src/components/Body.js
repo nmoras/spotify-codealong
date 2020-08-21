d1513ffd992ef02c486bea32c93579dadc60694a
poem <normamoras@gmail.com>
2020-08-21 09:55:33 -0400

    shows song list

--- spotify-clone-app/src/components/Body.js
+++ spotify-clone-app/src/components/Body.js
@@ -3,6 +3,8 @@ import { useDataLayerValue } from '../DataLayer'
 import PlayCircleFilledIcon from '@material-ui/icons/PlayCircleFilled';
 import FavoriteIcon from '@material-ui/icons/Favorite';
 import MoreHorizIcon from '@material-ui/icons/MoreHoriz';
+import SongRow from './SongRow';
+
 
 function Body( {spotify} ) {
     const [{ discover_weekly}, dispatch] = useDataLayerValue();
--- spotify-clone-app/src/components/SongRow.js
+++ spotify-clone-app/src/components/SongRow.js
@@ -1,9 +1,17 @@
 import React from 'react'
+import "./SongRow.css"
 
-function SongRow() {
+function SongRow( {track} ) {
     return (
-        <div>
-            
+        <div className="songRow">
+            <img className="songRow_album" src="" alt="" />
+            <div className="songRow_info">
+                <h1>{track.name}</h1>
+                <p> 
+                    {track.artists.map( (artist) => artists.name)}
+                    {track.album.name}
+                </p>
+            </div>     
         </div>
     )
 }
