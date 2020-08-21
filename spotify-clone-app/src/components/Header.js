e8ed7d733705d095583b986cc382c42029ed4425
poem <normamoras@gmail.com>
2020-08-21 11:41:41 -0400

    sidebar css update

--- spotify-clone-app/src/components/Header.js
+++ spotify-clone-app/src/components/Header.js
@@ -4,7 +4,7 @@ import { Avatar } from '@material-ui/core';
 import SearchIcon from '@material-ui/icons/Search';
 import { useDataLayerValue } from '../DataLayer';
 
-function Header() {
+function Header( {spotify} ) {
     const [{user}, dispatch] = useDataLayerValue();
 
     return (
@@ -12,16 +12,16 @@ function Header() {
             <div className='header_left'>
                 <SearchIcon />
                 <input 
-                    placeholder="Search for Artists, Songs"
+                    placeholder="Search for Artists, Songs"s
                     type="text"
                 />
             </div>
             <div className='header_right'>
-                <Avatar src="{user?.images[0]?.url}" alt="{user?.display_name}" />
+                <Avatar src={user?.images[0]?.url} alt={user?.display_name}/>
                 <h4>{user?.display_name}</h4>
             </div>
         </div>
-    )
+    );
 }
 
 export default Header
