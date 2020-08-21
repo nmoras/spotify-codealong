2bf851a5932dbcdecdca00d649d43df9abc6761a
poem <normamoras@gmail.com>
2020-08-21 12:00:37 -0400

    updated songrow component

--- spotify-clone-app/src/components/Sidebar.js
+++ spotify-clone-app/src/components/Sidebar.js
@@ -5,11 +5,13 @@ import HomeIcon from '@material-ui/icons/Home';
 import SearchIcon from '@material-ui/icons/Search';
 import LibraryMusicIcon from '@material-ui/icons/LibraryMusic';
 import { useDataLayerValue } from '../DataLayer';
-
+import { getTokenFromUrl } from '../spotify';
 
 
 function Sidebar() {
     const [{ playlists }, dispatch] = useDataLayerValue();
+    console.log(playlists)
+
     return (
         <div className="sidebar">
             <img className="sidebar_logo" src="https://getheavy.com/wp-content/uploads/2019/12/spotify2019-830x350.jpg"
@@ -19,14 +21,15 @@ function Sidebar() {
             <SidebarOption Icon={HomeIcon} title="Home" />
             <SidebarOption Icon={SearchIcon} title="search" />
             <SidebarOption Icon={LibraryMusicIcon} title="Your Library" />
+            <br/>
             <strong className="sidebar_title">PLAYLIST</strong>
             <hr/>
-            { playlists?.items?.map(playlist => (
+            {playlists?.items?.map( (playlist) => (
                 <SidebarOption title={playlist.name} />
             ))}
             
         </div>
-    )
+    );
 }
 
 export default Sidebar
