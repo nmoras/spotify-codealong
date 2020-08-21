50008d54767e069c30e17653aea564db6eebf141
poem <normamoras@gmail.com>
2020-08-21 07:05:03 -0400

    header info box

--- spotify-clone-app/src/components/Body.js
+++ spotify-clone-app/src/components/Body.js
@@ -4,7 +4,18 @@ function Body( {spotify} ) {
     return (
         <div className="body">
             <Header  spotify={spotify} />
-            
+
+            <div className="body_info">
+                <img 
+                    src="" 
+                    alt="" 
+                />
+                <div className="body_infoText">
+                    <strong> </strong>
+                    <h2>Discover Weekly</h2>
+                    <p>Description</p>
+                </div>        
+            </div>       
         </div>
     )
 }
