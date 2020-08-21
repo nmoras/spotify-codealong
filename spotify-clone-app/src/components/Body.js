59a0cacccc659659b0e5acb03387ad8787761553
poem <normamoras@gmail.com>
2020-08-21 07:44:03 -0400

    body info content

--- spotify-clone-app/src/components/Body.js
+++ spotify-clone-app/src/components/Body.js
@@ -1,24 +1,40 @@
 import React from 'react'
 import { useDataLayerValue } from '../DataLayer'
+import PlayCircleFilledIcon from '@material-ui/icons/PlayCircleFilled';
+import FavoriteIcon from '@material-ui/icons/Favorite';
+import MoreHorizIcon from '@material-ui/icons/MoreHoriz';
 
 function Body( {spotify} ) {
     const [{ discover_weekly}, dispatch] = useDataLayerValue();
-    
+
     return (
         <div className="body">
             <Header  spotify={spotify} />
 
             <div className="body_info">
                 <img 
-                    src="" 
+                    src="{discover_weekly?.images[0].url}" 
                     alt="" 
                 />
                 <div className="body_infoText">
                     <strong> </strong>
                     <h2>Discover Weekly</h2>
-                    <p>Description</p>
+                    <p>{discover_weekly?.description}</p>
                 </div>        
-            </div>       
+            </div>   
+
+            <div className="body_songs">
+                <div className="body_icons">
+                    <PlayCircleFilledIcon className="body_shuffle"/>
+                    <FavoriteIcon fontSize="large"/>
+                    <MoreHorizIcon />
+                </div>
+            
+            {discover_weekly?.tracks.items.Map( item => {
+                <SongRow track={item.track} />
+            })}
+            </div>   
+            <div className="body_songs"></div>     
         </div>
     )
 }
