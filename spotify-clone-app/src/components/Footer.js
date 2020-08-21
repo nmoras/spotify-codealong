db43ab0ebfc94dc8fd0409feabf331986f7e7479
poem <normamoras@gmail.com>
2020-08-21 05:31:30 -0400

    shows footer

--- spotify-clone-app/src/components/Footer.js
+++ spotify-clone-app/src/components/Footer.js
@@ -4,7 +4,9 @@ import './Footer.css'
 function Footer() {
     return (
         <div className="footer">
-
+            <div className="footer_left"></div>
+            <div className="footer_center"></div>
+            <div className="footer_right"></div>
             
         </div>
     )
