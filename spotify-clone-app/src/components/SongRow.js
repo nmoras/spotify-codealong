116ab1742451eac49f8778ae8ab931b6c2075d1e
poem <normamoras@gmail.com>
2020-08-21 12:14:10 -0400

    updated songrow component

--- spotify-clone-app/src/components/SongRow.js
+++ spotify-clone-app/src/components/SongRow.js
@@ -2,13 +2,15 @@ import React from 'react'
 import "./SongRow.css"
 
 function SongRow( {track} ) {
+    const [{ artists }, dispatch] = useDataLayerValue();
+    console.log(track);
     return (
         <div className="songRow">
-            <img className="songRow_album" src="" alt="" />
+            <img className="songRow_album" src={track.album.images[0].url} alt="" />
             <div className="songRow_info">
                 <h1>{track.name}</h1>
                 <p> 
-                    {track.artists.map( (artist) => artists.name)}
+                    {track.artists.map((artist) => artists.name).join(",")} -{" "}
                     {track.album.name}
                 </p>
             </div>     
